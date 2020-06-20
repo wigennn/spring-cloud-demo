89fc68aa657745321fb9ee67f29cecdf06571f04
18070900 <zett96@163.com>
2020-06-20 16:47:22 +0800

    auth init

--- auth-center/auth-api/src/test/java/com/wigen/authapi/AuthApiApplicationTests.java
+++ auth-center/auth-api/src/test/java/com/wigen/authapi/AuthApiApplicationTests.java
@@ -1,8 +1,13 @@
 package com.wigen.authapi;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
 
 }
